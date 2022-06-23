0e77285d1cd788700b73237702fd6ec588b55016
yigithankaraman <yigithankaraman@hotmail.com>
2022-06-23 15:56:59 +0300

    Add Remag Page

---
+++ RemarkPage/RemarkPage/MainPage.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace RemarkPage
+{
+    public partial class MainPage : ContentPage
+    {
+        bool btn = false;
+        /*
+        Rectangle expandedRect; // bounds for expanded image view
+        Rectangle detailsRect;  // bounds for details image view
+        */
+        public MainPage()
+        {
+            InitializeComponent();
+            imageAction.ScaleTo(1.05);
+        }
+
+        private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
+        {
+            if (btn)
+            {
+                await DetailPage.TranslateTo(0, 0, 600, Easing.BounceOut);
+                await RemarkPage.TranslateTo(-300, 40, 300,Easing.CubicInOut);
+                await HeaderPage.TranslateTo(-200, 0, 200);
+                await imageAction.ScaleTo(1.05, 100);
+
+                btn = false;
+                DetailPage.IsVisible = false;
+            }
+            else
+            {
+                await imageAction.ScaleTo(1, 100);
+                await HeaderPage.TranslateTo(0, 0, 200);
+                await RemarkPage.TranslateTo(0, 40, 300,Easing.CubicInOut);
+                DetailPage.IsVisible = true;
+                await DetailPage.TranslateTo(0, -40, 600, Easing.BounceOut);
+                btn = true;
+            }
+            
+
+        }
+
+        private void TapGestureRecognizer_Tapped_1(object sender, EventArgs e)
+        {
+            DisplayAlert("Uyarı", "Deneme Amaçlı Uyarı", "Devam");
+        }
+    }
+}
